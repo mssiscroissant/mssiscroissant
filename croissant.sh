5961274e9166e8481d1a263040cae936184c079a
mssiscroissant <mssiscroissant@outlook.com>
2024-10-25 16:16:57 +0200

    Update croissant.sh
    Modification get txt from git

--- croissant.sh
+++ croissant.sh
@@ -5,14 +5,21 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-if [ ! -f "$1" ]; then
-  echo "Le fichier $1 n'existe pas."
-  exit 1
+if [[ "$1" =~ ^https?:// ]]; then
+
+  input_command="curl -s $1"
+else
+  if [ ! -f "$1" ]; then
+    echo "Le fichier $1 n'existe pas."
+    exit 1
+  fi
+  input_command="cat $1"
 fi
 
 scroll() {
   local lines
   lines=$(tput lines)
+  
   while IFS= read -r line; do
     echo "$line"
     sleep 0.09
@@ -20,13 +27,12 @@ scroll() {
       sleep 0.5
       clear
     fi
-  done < "$1"
+  done < <($input_command) 
 }
 
 trap '' SIGINT SIGTSTP
 stty -echoctl
 clear
-while true;
-do
-	scroll "$1"
+while true; do
+  scroll
 done
